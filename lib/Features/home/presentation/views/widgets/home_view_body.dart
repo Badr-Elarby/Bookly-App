26ddcdd97910c28b6cb1154bc48567c46da96399
Badr-Elarby <badrshahd291@gmail.com>
2025-04-11 16:53:41 +0200

    add custom list view item to home

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly/Features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:bookly/Features/home/presentation/views/widgets/custom_list_view.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -9,7 +10,10 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
-      children: [CustomAppBar()],
+      children: [
+        CustomAppBar(),
+        CustomListViewItem(),
+      ],
     );
   }
 }
--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,3 +1,4 @@
 class AssetsData {
   static const logo = 'assets/images/images-removebg-preview.png';
+  static const testImage = 'assets/images/image.png';
 }
