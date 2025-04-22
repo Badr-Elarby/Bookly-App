d7ec992ca7a170bb5fea23f7731c3a8166253641
Badr-Elarby <badrshahd291@gmail.com>
2025-04-22 14:28:22 +0200

    create books action

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,8 +1,10 @@
 import 'package:bookly/Features/home/presentation/views/widgets/book_rating.dart';
+import 'package:bookly/Features/home/presentation/views/widgets/books_action.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/featuedListView.dart';
 import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/core/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -20,12 +22,12 @@ class BookDetailsViewBody extends StatelessWidget {
                 horizontal: MediaQuery.of(context).size.width * .18),
             child: const CustomBookImage(),
           ),
-          const SizedBox(height: 25),
+          const SizedBox(height: 15),
           const Text(
             'The Jungle Book',
             style: Styles.textStyle30,
           ),
-          const SizedBox(height: 5),
+          const SizedBox(height: 1),
           Opacity(
             opacity: .7,
             child: Text(
@@ -36,10 +38,12 @@ class BookDetailsViewBody extends StatelessWidget {
               ),
             ),
           ),
-          const SizedBox(height: 5),
+          const SizedBox(height: 1),
           const BookRating(
             mainAxisAlignment: MainAxisAlignment.center,
           ),
+          const SizedBox(height: 30),
+          const BooksAction(),
         ],
       ),
     );
