2803513965fbebd8cbbf5799fb0ff97a26af3b82
Badr-Elarby <badrshahd291@gmail.com>
2025-04-23 16:22:27 +0200

    create custom search text field

---
+++ lib/Features/search/presentation/views/widgets/custom_serach_text_field.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomSerachTextField extends StatelessWidget {
+  const CustomSerachTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        enabledBorder: BuildOutlineInputBorder(),
+        focusedBorder: BuildOutlineInputBorder(),
+        hintText: 'search',
+        suffixIcon: IconButton(
+          onPressed: () {},
+          icon: const Opacity(
+            opacity: 0.8,
+            child: Icon(
+              FontAwesomeIcons.magnifyingGlass,
+              size: 18,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  OutlineInputBorder BuildOutlineInputBorder() {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(12),
+      borderSide: const BorderSide(color: Color.fromARGB(255, 207, 198, 198)),
+    );
+  }
+}
--- lib/Features/search/presentation/views/widgets/search_view_body.dart
+++ lib/Features/search/presentation/views/widgets/search_view_body.dart
@@ -1,10 +1,21 @@
+import 'package:bookly/Features/search/presentation/views/widgets/custom_serach_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: 25,
+      ),
+      child: const Column(
+        children: [
+          CustomSerachTextField(),
+        ],
+      ),
+    );
   }
 }
