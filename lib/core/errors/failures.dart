24bd0c169bffdf8d67031a5b31aa30fe24f508ff
Badr-Elarby <badrshahd291@gmail.com>
2025-04-24 18:22:59 +0200

    create server failure form dio error

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,3 +1,47 @@
-abstract class Failure {}
+import 'package:dio/dio.dart';
 
-class ServerFailure implements Failure {}
+abstract class Failure {
+  final String errMessage;
+
+  const Failure(this.errMessage);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errMessage);
+
+  factory ServerFailure.fromDioError(DioError dioError) {
+    switch (dioError.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection timeout with ApiServer');
+
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send timeout with ApiServer');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive timeout with ApiServer');
+      case DioExceptionType.badCertificate:
+      // TODO: Handle this case.
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+            dioError.response!.statusCode!, dioError.response!.data);
+      // TODO: Handle this case.
+      case DioExceptionType.cancel:
+        return ServerFailure('Request canceled');
+      case DioExceptionType.connectionError:
+        return ServerFailure('No Internet connection');
+      case DioExceptionType.unknown:
+        return ServerFailure('Unknown error');
+    }
+  }
+
+  factory ServerFailure.fromResponse(int? statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(response['error']['message']);
+    } else if (statusCode == 404) {
+      return ServerFailure('your request Not Found');
+    } else if (statusCode == 500) {
+      return ServerFailure('Internal Server Error');
+    } else {
+      return ServerFailure('Unknown Error');
+    }
+  }
+}
